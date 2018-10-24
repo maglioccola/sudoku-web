dfb30695fe9d91d9a1730e4cec9ae268b30bced0
Alessandro Maglioccola <alessandro@maglioccola.com>
2018-10-24 22:18:46 +0200

    First commit, render grid from ajax request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+    render() {
+        if (this.props.value === 0) {
+            //TODO: Editable cell
+            return (
+                <button className="square"></button>
+            );
+        }
+        else { 
+            return (
+                <button className="square">
+                    {this.props.value}
+                </button>
+            );
+        }
+        
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            rows: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8090/api/generate")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        rows: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    renderSquare(key, value) {
+        return <Square key={key} value={value} />;
+    }
+
+    render() {
+        const { error, isLoaded, rows } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    {
+                        rows.map((cols, rowIndex) => (
+                            <div key={rowIndex} className="board-row">
+                                {
+                                    cols.map((cell, colIndex) => (
+                                        this.renderSquare(rowIndex * 10 + colIndex, cell)
+                                    ))
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
+            );
+        }
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
